865aaa8797367c859a6e801041ecd6e07c304ad0
Rosamund <rosamund.pearce@carbonbrief.org>
2019-05-06 12:14:55 +0100

    Chord generated :construction:

--- js/viz.js
+++ js/viz.js
@@ -1,38 +1,123 @@
 // create dataset from selection
 
-let random = [0,0,10,20,100,0,40,0,60,10,5,0,0,10,15,80,0,0,30,0,50];
-const keys = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh"];
+const random = [0,0,10,20,100,0,40,0,60,10,5,0,0,10,15,80,0,0,30,0,50];
+const keys = ["first", "second", "third", "fourth", "fifth", "sixth"];
+const colors = ["#51addf", "#c582aa", "#005b9d", "#35a993", "#cc373c", "#f7d783"];
 
-let arr = {};
+// let arr = {};
 
-function updateArray () {
-    for (var i = 0; i < 7; i++) {
-        let key = keys[i];
+// function updateArray () {
+//     for (var i = 0; i < 6; i++) {
+//         let key = keys[i];
 
-        arr[key] = "[" + random[Math.floor(Math.random() * random.length)] + "," 
-        + random[Math.floor(Math.random() * random.length)] + "," 
-        + random[Math.floor(Math.random() * random.length)] + "," 
-        + random[Math.floor(Math.random() * random.length)] + "," 
-        + random[Math.floor(Math.random() * random.length)] + "," 
-        + random[Math.floor(Math.random() * random.length)] + "," 
-        + random[Math.floor(Math.random() * random.length)] + "]";
+//         arr[key] = "[" + parseInt(random[Math.floor(Math.random() * random.length)])  + "," 
+//         + parseInt(random[Math.floor(Math.random() * random.length)]) + "," 
+//         + parseInt(random[Math.floor(Math.random() * random.length)])  + "," 
+//         + parseInt(random[Math.floor(Math.random() * random.length)])  + "," 
+//         + parseInt(random[Math.floor(Math.random() * random.length)])  + "," 
+//         + parseInt(random[Math.floor(Math.random() * random.length)])  + "]";
 
-    }
+//     }
+// }
+
+// updateArray();
+
+// var matrix = Object.values(arr);
+// var matrixKeys = Object.keys(arr);
+
+
+var matrix = [
+    [0, 293, 374, 380, 132, 128], // X
+    [251, 0, 285, 197, 398, 161], // Y
+    [403, 426, 0, 200,  198, 141], // Z
+    [454, 267, 291, 0, 294, 341], // C
+    [200, 665, 403, 326, 0, 286], // B
+    [210, 251, 211, 289, 247, 0] // A
+];
+
+console.log(matrix);
+
+var width = parseInt(d3.select("#viz").style("width")),
+    height = width;
+    outerRadius = Math.min(width, height) * 0.5 - 150,
+    innerRadius = outerRadius - 30;
+
+var svg = d3.select("#viz").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+var wrapper = svg.append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ") rotate(75)");
+
+var chordGenerator = d3.chord()
+    .padAngle(0.1)
+    .sortSubgroups(d3.descending)
+    .sortChords(d3.descending);
+
+var chord = chordGenerator(matrix);
+
+var arcs = d3.arc()
+    .innerRadius(innerRadius)
+    .outerRadius(outerRadius);
+
+var ribbon = d3.ribbon()
+    .radius(250);
+
+var color = d3.scaleOrdinal()
+    .domain(d3.range(6))
+    .range(colors);
+
+function getGradID(d){ 
+    return "linkGrad-" + d.source.index + "-" + d.target.index; 
 }
 
-updateArray();
-console.log(arr);
+var grads = svg.append("defs")
+.selectAll("linearGradient")
+.data(chord)
+.enter()
+.append("linearGradient")
+.attr("id", getGradID)
+.attr("gradientUnits", "userSpaceOnUse")
+.attr("x1", function(d, i){ return innerRadius * Math.cos((d.source.endAngle-d.source.startAngle) / 2 + d.source.startAngle - Math.PI/2); })
+.attr("y1", function(d, i){ return innerRadius * Math.sin((d.source.endAngle-d.source.startAngle) / 2 + d.source.startAngle - Math.PI/2); })
+.attr("x2", function(d,i){ return innerRadius * Math.cos((d.target.endAngle-d.target.startAngle) / 2 + d.target.startAngle - Math.PI/2); })
+.attr("y2", function(d,i){ return innerRadius * Math.sin((d.target.endAngle-d.target.startAngle) / 2 + d.target.startAngle - Math.PI/2); });
 
-var margin = {top: 20, right: 20, bottom: 20, left: 20},
-    width = parseInt(d3.select("#line-wrapper").style("width")) - margin3.left - margin3.right,
-    height = width - margin3.top - margin3.bottom;
+// set the starting color (at 0%)
+grads.append("stop")
+    .attr("offset", "0%")
+    .attr("stop-color", function(d){ return color(d.source.index)});
 
-var svg3 = d3.select("#viz").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+//set the ending color (at 100%)
+grads.append("stop")
+    .attr("offset", "100%")
+    .attr("stop-color", function(d){ return color(d.target.index)});
+
+// making the ribbons
+d3.select("g")
+    .selectAll("path")
+    .data(chord)
+    .enter()
+    .append("path")
+    .attr("class", function(d) {
+    return "chord chord-" + d.source.index + " chord-" + d.target.index // The first chord allows us to select all of them. The second chord allows us to select each individual one. 
+    })
+    .style("fill", function(d){ return "url(#" + getGradID(d) + ")"; })
+    .attr("d", ribbon);
+
+// making the arcs
+var g = wrapper.selectAll("g")
+    .data(chord.groups)
+    .enter()
     .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("class", "group");  
 
+
+g.append("path")
+    .style("fill", function(d){ return color(d.index)})
+    // .style("stroke", function(d){ return d3.rgb(color(d.index)).darker(); })
+    .attr("d", arcs)
+    .style("opacity", 1);
 
 
 
